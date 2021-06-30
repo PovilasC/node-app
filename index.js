b97b33a9041e08bd70858b03453df5c7d694e4e1
Povilas <polass47@gmail.com>
2021-06-30 16:30:50 +0300

    dffd

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express');
 
 const app = new express()
 
-app.get('/', (req, res, next)=> {
-  res.write('<h1>Hello there</h1>');
-
+app.get('/', (req, res, next) => {
+  res.write(`
+  <h1>Hello there</h1>
+  <h2>Hello there</h2>
+  `);
+  res.end()
 })
 
-app.listen(3000, ()=> {
+app.listen(3000, () => {
   console.log('listening on port 3000')
 })
